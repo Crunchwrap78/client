57ed9620985df975bee8be8dd03035e6f5c693d3
Crucnhwrap78 <Hayesapples78@gmail.com>
2018-01-06 14:30:26 -0500

    baseline complete

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import App from './components/App';
+
+const jsx = <App />;
+
+render(jsx, document.getElementById('app'));
---
+++ webpack.config.babel.js
@@ -0,0 +1,48 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const paths = {
+  app: './src/index.js',
+  build: path.join(__dirname, 'public')
+}
+
+const base = {
+  entry: [
+    paths.app
+  ],
+  output: {
+    path: paths.build,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {test: /\jsx?$/, exclude:/node_modules/, loader: 'babel-loader'},
+      {test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader'}
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
+
+const productionCheck = process.env.npm_lifecycle_event;
+const isProduction = productionCheck === 'production';
+process.env.BABEL_ENV = productionCheck;
+
+const productionPlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+});
+
+const developmentConfig = {
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    inline: true,
+    progress: true,
+  },
+}
+
+export default Object.assign({}, base,
+  isProduction === true ? productionConfig : developmentConfig
+);
